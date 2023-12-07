7e20a8ddeeeb4dd1def80151a2506a5a3c285a52
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-07 17:08:41 +0100

    challenge solved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+/* import logo from "./logo.svg"; */
+import "./App.css";
+import React, { useState, useEffect } from "react";
+/* import { act } from "react-dom/test-utils"; */
 
 function App() {
+  const [numberInSpace, setNumberInSpace] = useState(0);
+  const [peopleInSpace, setPeopleInSpace] = useState([]);
+  const [activeCraft, setActiveCraft] = useState("All");
+
+  useEffect(() => {
+    async function fetchPeopleInSpace() {
+      const response = await fetch(`http://api.open-notify.org/astros.json`);
+      const data = await response.json();
+
+      setNumberInSpace(data.number);
+      setPeopleInSpace(data.people);
+    }
+
+    fetchPeopleInSpace();
+  }, []);
+  const filteredPeople =
+    activeCraft === "All"
+      ? peopleInSpace
+      : peopleInSpace.filter((person) => person.craft === activeCraft);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {filteredPeople.length > 0 && (
+        <ul>
+          {filteredPeople.map((person) => (
+            <li key={person.name}>{person.name}</li>
+          ))}
+        </ul>
+      )}
+
+      <h2>Craft: {activeCraft}</h2>
+      <button type="button" onClick={() => setActiveCraft("All")}>
+        All
+      </button>
+
+      <button type="button" onClick={() => setActiveCraft("ISS")}>
+        ISS
+      </button>
+      <button type="button" onClick={() => setActiveCraft("Tiangong")}>
+        Tiangong
+      </button>
+    </main>
   );
 }
 
